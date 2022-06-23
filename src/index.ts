428e4d82789503f03923d229dbd69ee62fc77f02
swensson <lxswensson@gmail.com>
2022-06-23 08:54:57 +0200

    Update index.ts accordingly

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 // Import plugins
 import CanvasRender from './plugins/core/canvas-render';
 import CanvasEvents from './plugins/core/canvas-events';
-
 import RenderWindow from './plugins/core/render-window';
+import SimulationTicks from './plugins/core/simulation-ticks';
 
+import BrainsManager from './plugins/simulation/brains-manager';
 import CellsManager from './plugins/simulation/cells-manager';
-import SunlightManager from './plugins/simulation/sunlight-manager';
+
+import Movements from './plugins/extensions/movements';
+import Food from './plugins/extensions/food';
+import Reproduce from './plugins/extensions/reproduce';
+// import FoodActions from './plugins/simulation/food-actions';
+// import SunlightManager from './plugins/simulation/sunlight-manager';
 
 // Kickoff the app
 export default (node: HTMLElement) => {
@@ -13,7 +19,17 @@ export default (node: HTMLElement) => {
   const canvasEvents = new CanvasEvents(node);
 
   const renderWindow = new RenderWindow(canvasRender, canvasEvents);
+  const ticks = new SimulationTicks(canvasRender);
 
-  const sunlight = new SunlightManager(renderWindow);
-  const cells = new CellsManager(renderWindow);
+  // const sunlight = new SunlightManager(renderWindow);
+  const brains = new BrainsManager();
+  const cells = new CellsManager(renderWindow, ticks, brains);
+
+  const food = new Food(cells, renderWindow, ticks);
+  const movements = new Movements(cells, brains, food);
+
+  const reproduce = new Reproduce(cells, food, brains);
+  // const basic = new BasicActions(cells);
+  // const food = new FoodActions(cells, renderWindow);
+
 };
