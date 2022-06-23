153f475d5fe66bae92961ba15003fe5f6310f665
swensson <lxswensson@gmail.com>
2022-06-23 17:03:15 +0200

    Include photosynthesis

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,17 @@ import SimulationTicks from './plugins/core/simulation-ticks';
 import BrainsManager from './plugins/simulation/brains-manager';
 import CellsManager from './plugins/simulation/cells-manager';
 
-import Movements from './plugins/extensions/movements';
-import Food from './plugins/extensions/food';
-import Reproduce from './plugins/extensions/reproduce';
+// import Movements from './plugins/extensions/movements';
+// import Food from './plugins/extensions/food';
+// import Reproduce from './plugins/extensions/reproduce';
+// import Vision from './plugins/extensions/vision';
+
+import Photosynthesis from './plugins/extensions/photosynthesis';
+
+import ReactCore from './plugins/interface/react-core';
+import WindowsManager from './plugins/interface/windows-manager';
+// import MainStats from './plugins/interface/main-stats';
+import CellView from './plugins/interface/cell-view';
 
 export default (node: HTMLElement) => {
   const canvasRender = new CanvasRender(node);
@@ -20,7 +28,15 @@ export default (node: HTMLElement) => {
   const brains = new BrainsManager();
   const cells = new CellsManager(renderWindow, ticks, brains);
 
-  const food = new Food(cells, renderWindow, ticks);
-  const movements = new Movements(cells, brains, food);
-  const reproduce = new Reproduce(cells, food, brains);
+  const photosynthesis = new Photosynthesis(renderWindow, ticks, cells, brains);
+
+  // const food = new Food(cells, renderWindow, ticks);
+  // const movements = new Movements(cells, brains, food);
+  // const reproduce = new Reproduce(cells, food, brains);
+  // const vision = new Vision(brains, cells, food);
+
+  const core = new ReactCore(node);
+  const windows = new WindowsManager(core);
+  // const stats = new MainStats(windows, cells);
+  const view = new CellView(windows, cells, renderWindow, canvasEvents, brains);
 };
