93d9c1c7831e87f342c6f7e0439981afc4fffc87
swensson <lxswensson@gmail.com>
2022-06-25 11:29:33 +0200

    Refactor cells manager

--- src/plugins/simulation/cells-manager/index.ts
+++ src/plugins/simulation/cells-manager/index.ts
@@ -5,76 +5,60 @@ import { Point } from '/src/plugins/core/canvas-events';
 import RenderWindow from '/src/plugins/core/render-window';
 import SimulationTicks from '/src/plugins/core/simulation-ticks';
 
-import BrainsManager from '/src/plugins/simulation/brains-manager';
+import BrainsManager, { Brain } from '/src/plugins/simulation/brains-manager';
 
 const GRID_SIZE = 20; // TODO GRID SIZE global constant
 const CELL_COLOR = '#2196f3';
 
+export type Cell = Point & { brainId: number };
+
 export default class CellsManager {
-  private idgenerator = 1;
+  private idgen = 1;
+  public cells = new Map<number, Cell>();
 
   public onNewCell = new EventEmitter<number>();
   public onRemoveCell = new EventEmitter<number>();
 
-  public cells = [{ id: 0, x: 10, y: 10, brainId: 0 }];
+  public spawn = (p: Point, brainId: number) => { const id = this.idgen++; this.cells.set(id, { ...p, brainId }); return id; };
+  public despawn = (id: number) => this.cells.delete(id);
+  public get = (id: number) => this.cells.get(id);
+  public update = (id: number, u: (cell: Cell) => Cell) => { const cell = this.get(id); if (cell) this.cells.set(id, u(cell)); };
+  public ids = () => this.cells.keys();
 
-  public add = (p: Point, brainId: number) => {
-    const id = this.idgenerator++;
-    this.cells.push({ ...p, id, brainId });
-    // this.onNewCell.emitps(id);
-    return id;
-  }
-  public get = (id: number) => this.cells.find((c) => c.id === id);
-  public update = (id: number, u) => this.cells = this.cells.map((c) => c.id === id ? u(c) : c);
-  public remove = (id: number) => {
-    this.cells = this.cells.filter((c) => c.id !== id);
-    // this.onRemoveCell.emitps(id);
-  }
-  public ids = () => this.cells.map((c) => c.id);
+  public onCollectClues = new EventEmitter();
+  public onSubmitAction = new EventEmitter();
+  public onAfterAction  = new EventEmitter();
+  // public onDataOutput = new EventEmitter();
+  // public onAfterTick = new EventEmitter();
 
   constructor (private window: RenderWindow, private ticks: SimulationTicks, private brains: BrainsManager) {
     // Setup cells rendering
-    this.window.onRender.on(this.render);
+    this.window.onRender.on((context: CanvasRenderingContext2D) => {
+      context.fillStyle = CELL_COLOR;
+
+      this.cells.forEach((cell) => {
+        context.beginPath();
+        context.rect(cell.x * GRID_SIZE, cell.y * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+        context.fill();
+      });
+    });
 
     // Setup cells thinking
-    this.ticks.add({ interval: 200, color: CELL_COLOR }, this.think);
-  };
+    this.ticks.add({ interval: 200, color: CELL_COLOR }, () => {
+      for (let [id, cell] of this.cells.entries()) {
+        const clues = this.onCollectClues.emitps(id);
+        const actions = this.brains.tick(cell.brainId, clues);
 
-  // Render cells
-  private render = (context: CanvasRenderingContext2D) => {
-    context.fillStyle = CELL_COLOR;
-
-    this.cells.forEach((cell) => {
-      context.beginPath();
-      context.rect(cell.x * GRID_SIZE, cell.y * GRID_SIZE, GRID_SIZE, GRID_SIZE);
-      context.fill();
-    });
-  };
-
-  // Handle ticks of life
-
-  public onDataInput = new EventEmitter();
-  public onDataOutput = new EventEmitter();
-  public onAfterTick = new EventEmitter();
-
-  private think = () => {
-    this.cells.forEach((cell) => {
-      const inputs = this.onDataInput.emitps(cell.id);
-      const outputs = this.brains.tick(cell.brainId, inputs);
-
-
-
-      if (outputs.length === 0) {
-        // this.cells.remove
-        // console.log('braindead', cell.id, cell.brainId, this.brains.get(cell.brainId))
-        this.remove(cell.id);
-      } else {
-        outputs.forEach((action) => this.onDataOutput.emitps({ id: cell.id, action }));
-        this.onAfterTick.emitps(cell.id); // do the post action handling for food eating and dying and other stuff
+        // if (actions.length === 0)  kill cell
+        actions.forEach((action) => this.onSubmitAction.emitps({ id, action }));
+        this.onAfterAction.emitps(id);
       }
-
-
-      // console.log(this.cells)
     });
+
+    // Create the very first cell
+    
+    const simpleBrainId = this.brains.save(new Brain(0, [{ from: 0, clues: [], to: 0 }], [['move/random', 'reproduce']]));
+
+    this.spawn({ x: 0, y: 0 }, simpleBrainId)
   };
 };
